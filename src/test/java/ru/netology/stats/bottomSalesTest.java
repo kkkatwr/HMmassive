95b2dc3c03fb5ce78fd62b6e031986676616f5f2
kkkatwr <Katyad1318@gmail.com>
2024-06-22 22:40:11 +0500

    Add test bottomSalesTest

--- src/test/java/ru/netology/stats/bottomSalesTest.java
+++ src/test/java/ru/netology/stats/bottomSalesTest.java
@@ -1,15 +1,18 @@
+
 package ru.netology.stats;
+
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class peakSalesTest {
+public class bottomSalesTest {
     @Test
     public void test() {
-        peakSales service = new peakSales();
-        int expected = 8;
+        bottomSales service = new bottomSales();
+        int expected = 9;
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actuall = service.maxSales(sales);
+        int actuall = service.minSales(sales);
         Assertions.assertEquals(expected, actuall);
     }
 }
+
